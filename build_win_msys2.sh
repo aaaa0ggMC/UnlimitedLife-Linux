0e6cfd1115066d9f802dc9e752017674955cff18
RockonDreaming <60734273+aaaa0ggMC@users.noreply.github.com>
2025-03-21 18:29:43 +0800

    Update build_win_msys2.sh

--- build_win_msys2.sh
+++ build_win_msys2.sh
@@ -1,1 +1,9 @@
 pacman -S --noconfirm --needed mingw-w64-ucrt-x86_64-glm mingw-w64-ucrt-x86_64-glfw mingw-w64-ucrt-x86_64-rapidjson mingw-w64-ucrt-x86_64-glew mingw-w64-ucrt-x86_64-tomlplusplus mingw-w64-ucrt-x86_64-cmake
+
+mkdir -p CBuild_CACHE_WIN
+cd CBuild_CACHE_WIN
+
+# Run CMake to configure the project
+cmake .. -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles"
+
+
