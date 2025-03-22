dd548add2da790a85cc5190a9424bc78e10d9d4f
RockonDreaming <60734273+aaaa0ggMC@users.noreply.github.com>
2025-03-22 12:53:40 +0800

    ++

--- configure_win_msys2.sh
+++ configure_win_msys2.sh
@@ -1,8 +1,9 @@
-pacman -S --noconfirm --needed git mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-glm mingw-w64-ucrt-x86_64-glfw mingw-w64-ucrt-x86_64-rapidjson mingw-w64-ucrt-x86_64-glew mingw-w64-ucrt-x86_64-tomlplusplus mingw-w64-ucrt-x86_64-cmake
+pacman -S --noconfirm --needed git mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-glm mingw-w64-ucrt-x86_64-glfw mingw-w64-ucrt-x86_64-rapidjson mingw-w64-ucrt-x86_64-glew mingw-w64-ucrt-x86_64-tomlplusplus mingw-w64-ucrt-x86_64-cmake mingw-w64-ucrt-x86_64-make
+mkdir -p CBuild_CACHE_WIN
+cd CBuild_CACHE_WIN
 
 # Run CMake to configure the project
 cmake .. -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles"
-
 
 
 
