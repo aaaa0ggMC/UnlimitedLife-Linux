756ff6699ed88ee4178f711330a60d406f4ea9c8
RockonDreaming <60734273+aaaa0ggMC@users.noreply.github.com>
2025-07-20 10:01:09 +0800

    小小优化
    windows那里我创建了符号链接，这样就不会上传两份文件了

--- scripts/configure_win_msys2.sh
+++ scripts/configure_win_msys2.sh
@@ -1,5 +1,5 @@
 # cd .. 工作目录就是 ./
-pacman -S --noconfirm --needed git mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-glm mingw-w64-ucrt-x86_64-glfw mingw-w64-ucrt-x86_64-rapidjson mingw-w64-ucrt-x86_64-glew mingw-w64-ucrt-x86_64-tomlplusplus mingw-w64-ucrt-x86_64-cmake mingw-w64-ucrt-x86_64-make mingw-w64-ucrt-x86_64-stb
+pacman -S --noconfirm --needed git mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-glm mingw-w64-ucrt-x86_64-glfw mingw-w64-ucrt-x86_64-rapidjson mingw-w64-ucrt-x86_64-glew mingw-w64-ucrt-x86_64-tomlplusplus mingw-w64-ucrt-x86_64-cmake mingw-w64-ucrt-x86_64-make mingw-w64-ucrt-x86_64-stb mingw-w64-ucrt-x86_64-ninja
 # @todo IMGUI编译
 mkdir -p CBuild_CACHE_WIN
 mkdir -p CBuild/Windows
@@ -32,14 +32,14 @@ cd CBuild_CACHE_WIN
 cp /ucrt64/lib/libglfw3.dll.a /ucrt64
 cp /ucrt64/lib/libglew32.dll.a /ucrt64
 
-# del .dll.as
-find /ucrt64/lib -name "*.dll.a" -delete
+# del .dll.as this may cause link problems
+# find /ucrt64/lib -name "*.dll.a" -delete
 
 cp /ucrt64/libglfw3.dll.a /ucrt64/lib/
 cp /ucrt64/libglew32.dll.a /ucrt64/lib/
 
 # Run CMake to configure the project
-cmake .. -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles"
+cmake .. -DCMAKE_BUILD_TYPE=Release -G "Ninja"
 
 
 
