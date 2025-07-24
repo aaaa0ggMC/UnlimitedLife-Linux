ca595cdc968c208619acddd65bf5ecec771df62b
RockonDreaming <60734273+aaaa0ggMC@users.noreply.github.com>
2025-07-24 19:23:30 +0800

    把hooks加上了

--- CDep/headers/AGE/Light.h
+++ CDep/headers/AGE/Light.h
@@ -1,3 +1,14 @@
+/**
+ * @file Light.h
+ * @author aaaa0ggmc (lovelinux@yslwd.eu.org)
+ * @brief 光照支持
+ * @version 0.1
+ * @date 2025/07/24
+ *
+ * @copyright Copyright(c)2025 aaaa0ggmc
+ * 
+ * @start-date 2025/07/24 
+ */
 #ifndef AGE_H_LIGHT
 #define AGE_H_LIGHT
 #include <AGE/Utils.h>
