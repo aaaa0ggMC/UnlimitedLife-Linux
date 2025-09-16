287d51c7de16c634d24c1c3141d1efa02e53f9a0
RockonDreaming <60734273+aaaa0ggMC@users.noreply.github.com>
2025-09-16 14:09:52 +0800

    WindowsAction出了点小问题

--- scripts/build_win_msys2.sh
+++ scripts/build_win_msys2.sh
@@ -3,13 +3,3 @@ mkdir -p CBuild_CACHE_WIN
 cd CBuild_CACHE_WIN
 
 ninja
-
-# Move Artifact
-mv ../CBuild/Windows/* ../CBuild/
-rmdir ../CBuild/Windows/
-
-#mov deps
-cp /ucrt64/bin/glfw3.dll ../CBuild/
-cp /ucrt64/bin/glew32.dll ../CBuild/
-cp /ucrt64/bin/libwinpthread-1.dll ../CBuild/
-cp /ucrt64/bin/libgcc_s_seh-1.dll ../CBuild/
